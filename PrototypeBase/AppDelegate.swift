85684e4c774f0662e6fe3156c4a6861255a9f37b
Bijit Halder <halderbijit@baidu.com>
2016-05-24 13:55:28 -0700

    last DS framework

--- PrototypeBase/AppDelegate.swift
+++ PrototypeBase/AppDelegate.swift
@@ -19,7 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
         
-        let mainViewController =  MapViewController()//ApiAiViewController() //ApiAiTextButtonViewController() //SpeechViewController()//MainViewController()
+        //let mainViewController =  MapViewController()//ApiAiViewController() //ApiAiTextButtonViewController() //SpeechViewController()//MainViewController()
+        let mainViewController = SpeechViewController()
         let navController: UINavigationController = UINavigationController(rootViewController: mainViewController)
         
         self.window!.rootViewController = navController
